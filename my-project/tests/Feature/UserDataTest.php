9d284a43f1e216bfd321f745f9ad2403df9fa6ec
booksey <booksey87@gmail.com>
2022-06-27 17:46:23 +0200

    Added a new test logic to UserDataTest

--- my-project/tests/Feature/UserDataTest.php
+++ my-project/tests/Feature/UserDataTest.php
@@ -2,6 +2,8 @@
 
 namespace Tests\Feature;
 
+use App\Classes\ExampleData;
+use App\Classes\UserData;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -13,10 +15,14 @@ class UserDataTest extends TestCase
      *
      * @return void
      */
-    public function test_example()
+    public function test_example_user_data_calculate()
     {
-        $response = $this->get('/');
-
-        $response->assertStatus(200);
+        $exampleData = ExampleData::get();
+        foreach ($exampleData as $data) {
+            $userData = new UserData($data);
+            $userData->calculate();
+            $this->assertIsInt($userData->getCalculatedStandardPoints());
+            $this->assertIsInt($userData->getCalculatedStandardPoints());
+        }
     }
 }
